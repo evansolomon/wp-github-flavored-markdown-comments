66c79dfd1810a53cbd35660c2cd66a0ef6761b16
Evan Solomon <evan@evanalyze.com>
2013-02-24 10:56:32 -0800

    Tabs, not spaces

--- github-flavored-markdown-comments.php
+++ github-flavored-markdown-comments.php
@@ -11,88 +11,88 @@ Author URI: http://evansolomon.me/
 
 // Wait to load the library in case another plugin tries to do the same but doesn't check
 add_action( 'init', function(){
-    // Base markdown library
-    if ( ! class_exists( 'Markdown_Parser' ) )
-        include( __DIR__ . '/lib/markdown.php' );
+	// Base markdown library
+	if ( ! class_exists( 'Markdown_Parser' ) )
+		include( __DIR__ . '/lib/markdown.php' );
 
-    // From `extra` branch of https://github.com/michelf/php-markdown/
-    if ( ! class_exists( 'MarkdownExtra_Parser' ) )
-        include( __DIR__ . '/lib/markdown-extra.php' );
+	// From `extra` branch of https://github.com/michelf/php-markdown/
+	if ( ! class_exists( 'MarkdownExtra_Parser' ) )
+		include( __DIR__ . '/lib/markdown-extra.php' );
 
-    /**
-     * Add a few extras from GitHub's Markdown implementation
-     * https://github.com/github/github-flavored-markdown
-     */
-    class ES_GHF_Markdown_Parser extends MarkdownExtra_Parser {
-        /**
-         * Overload to enable single-newline paragraphs
-         * https://github.com/github/github-flavored-markdown/blob/gh-pages/index.md#newlines
-         */
-        function formParagraphs( $text ) {
-            // Treat single linebreaks as double linebreaks
-            $text = preg_replace('#([^\n])\n([^\n])#', "$1\n\n$2", $text );
-            return parent::formParagraphs( $text );
-        }
+	/**
+	 * Add a few extras from GitHub's Markdown implementation
+	 * https://github.com/github/github-flavored-markdown
+	 */
+	class ES_GHF_Markdown_Parser extends MarkdownExtra_Parser {
+		/**
+		 * Overload to enable single-newline paragraphs
+		 * https://github.com/github/github-flavored-markdown/blob/gh-pages/index.md#newlines
+		 */
+		function formParagraphs( $text ) {
+			// Treat single linebreaks as double linebreaks
+			$text = preg_replace('#([^\n])\n([^\n])#', "$1\n\n$2", $text );
+			return parent::formParagraphs( $text );
+		}
 
-        /**
-         * Overload to support ```-fenced code blocks
-         * https://github.com/github/github-flavored-markdown/blob/gh-pages/index.md#fenced-code-blocks
-         */
-        function doCodeBlocks( $text ) {
-            $text = preg_replace_callback(
-                '#'       .
-                '^```'    . // Fenced code block
-                '[^\n]*$' . // No language-specific support yet
-                '\n'      . // Newline
-                '(.+?)'   . // Actual code here
-                '\n'      . // Last newline
-                '^```$'   . // End of block
-                '#ms',      // Multiline mode + dot matches newlines
-                array( $this, '_doCodeBlocks_callback' ),
-                $text
-            );
+		/**
+		 * Overload to support ```-fenced code blocks
+		 * https://github.com/github/github-flavored-markdown/blob/gh-pages/index.md#fenced-code-blocks
+		 */
+		function doCodeBlocks( $text ) {
+			$text = preg_replace_callback(
+				'#'       .
+				'^```'    . // Fenced code block
+				'[^\n]*$' . // No language-specific support yet
+				'\n'      . // Newline
+				'(.+?)'   . // Actual code here
+				'\n'      . // Last newline
+				'^```$'   . // End of block
+				'#ms',      // Multiline mode + dot matches newlines
+				array( $this, '_doCodeBlocks_callback' ),
+				$text
+			);
 
-            return parent::doCodeBlocks( $text );
-        }
+			return parent::doCodeBlocks( $text );
+		}
 
-    }
+	}
 } );
 
 class ES_GHF_Markdown_Comments {
-    protected $parser;
-    private static $instance;
+	protected $parser;
+	private static $instance;
 
-    public static function get_instance() {
-        if ( ! self::$instance )
-            self::$instance = new self;
+	public static function get_instance() {
+		if ( ! self::$instance )
+			self::$instance = new self;
 
-        return self::$instance;
-    }
+		return self::$instance;
+	}
 
-    private function __construct() {
-        add_action( 'init', array( $this, 'init' ) );
+	private function __construct() {
+		add_action( 'init', array( $this, 'init' ) );
 
-        // Just in case another plugin loads the Markdown lib with the WP stuff turned on
-        @define( 'MARKDOWN_WP_POSTS', false );
-        @define( 'MARKDOWN_WP_COMMENTS', false );
-    }
+		// Just in case another plugin loads the Markdown lib with the WP stuff turned on
+		@define( 'MARKDOWN_WP_POSTS', false );
+		@define( 'MARKDOWN_WP_COMMENTS', false );
+	}
 
-    public function init() {
-        // Make sure our conditional library loading worked above
-        if ( ! class_exists( 'ES_GHF_Markdown_Parser' ) )
-            return;
+	public function init() {
+		// Make sure our conditional library loading worked above
+		if ( ! class_exists( 'ES_GHF_Markdown_Parser' ) )
+			return;
 
-        if ( ! $this->parser )
-            $this->parser = new ES_GHF_Markdown_Parser;
+		if ( ! $this->parser )
+			$this->parser = new ES_GHF_Markdown_Parser;
 
-        // Markdown-ify comments
-        add_filter( 'pre_comment_content', array( $this, 'markdown' ) , 6  );
-        return $this;
-    }
+		// Markdown-ify comments
+		add_filter( 'pre_comment_content', array( $this, 'markdown' ) , 6  );
+		return $this;
+	}
 
-    public function markdown( $text ) {
-        return $this->parser->transform($text);
-    }
+	public function markdown( $text ) {
+		return $this->parser->transform($text);
+	}
 }
 
 ES_GHF_Markdown_Comments::get_instance();
