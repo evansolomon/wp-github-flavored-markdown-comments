7018ba94737dc5e2fee5981957d8f6d647dd6b7a
Evan Solomon <evan@evanalyze.com>
2013-02-24 09:56:50 -0800

    Add Github features to Markdown library
    * Single linebreaks get treated as paragraphs
    * ``` code fencing (Currently ignored language settings)

---
+++ markdown-comments.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+Plugin Name: Github-Flavored Markdown Comments
+Plugin URI: https://github.com/evansolomon/wp-github-flavored-markdown-comments
+Description: Markdown-powered WordPress comments, with a Github Twist
+Version: 1.0
+Author: Evan Solomon
+Author URI: http://evansolomon.me/
+*/
+
+// Markdown library
+// From `extra` branch of https://github.com/michelf/php-markdown/
+if ( ! class_exists( 'MarkdownExtra_Parser' ) )
+    include( __DIR__ . '/lib/markdown.php' );
+
+/**
+ * Add a few extras from Github's Markdown implementation
+ * https://github.com/github/github-flavored-markdown
+ */
+class ES_GH_Markdown_Parser extends MarkdownExtra_Parser {
+    /**
+     * Overload to enable single-newline paragraphs
+     * https://github.com/github/github-flavored-markdown/blob/gh-pages/index.md#newlines
+     */
+    function formParagraphs( $text ) {
+        // Treat single linebreaks as double linebreaks
+        $text = preg_replace('#([^\n])\n([^\n])#', "$1\n\n$2", $text );
+        return parent::formParagraphs( $text );
+    }
+
+    /**
+     * Overload to support ```-fenced code blocks
+     * https://github.com/github/github-flavored-markdown/blob/gh-pages/index.md#fenced-code-blocks
+     */
+    function doCodeBlocks( $text ) {
+        $text = preg_replace_callback(
+            '#'       .
+            '^```'    . // Fenced code block
+            '[^\n]*$' . // No language-specific support yet
+            '\n'      . // Newline
+            '(.+?)'   . // Actual code here
+            '\n'      . // Last newline
+            '^```$'   . // End of block
+            '#ms',      // Multiline mode + dot matches newlines
+            array( $this, '_doCodeBlocks_callback' ),
+            $text
+        );
+
+        return parent::doCodeBlocks( $text );
+    }
+
+}
